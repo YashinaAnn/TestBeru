9043655ea7b33f103aee92d230b03408eafd561c
Яшина Анна <annaya15@mail.ru>
2019-04-20 13:23:02 +0400

    added return to the previous page

--- testBeru/src/main/java/com/pack/pageobjects/SettingsPage.java
+++ testBeru/src/main/java/com/pack/pageobjects/SettingsPage.java
@@ -1,19 +1,25 @@
 package com.pack.pageobjects;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SettingsPage extends Page {
 	
-	private By city = By.xpath("//h2[text()='Ваш город ']/span/span");
+	private By city = By.xpath("//h2[contains(text(),'Ваш город ')]/span/span");
 	
 	public SettingsPage(WebDriver driver, WebDriverWait wait) {
 		super(driver, wait);
 	}
 	
 	public String getCity() {
-		this.scrollToElement(city);
-		return this.getText(city);
+		scrollToElement(city);
+		String currentCity = getText(city);
+		driver.close();
+		ArrayList<String> windows = new ArrayList<String> (driver.getWindowHandles());
+		driver.switchTo().window(windows.get(0));
+		return currentCity;
 	}
 }
