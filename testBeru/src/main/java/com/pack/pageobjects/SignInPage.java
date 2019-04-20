c8703888827a1b7b5c61cac071c11105818e5ffd
Яшина Анна <annaya15@mail.ru>
2019-04-20 13:20:18 +0400

    changed sign in method

--- testBeru/src/main/java/com/pack/pageobjects/SignInPage.java
+++ testBeru/src/main/java/com/pack/pageobjects/SignInPage.java
@@ -6,18 +6,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SignInPage extends Page {
 	
-	private By passwordBy = By.id("passp-field-passwd");
-	private By loginBy = By.id("passp-field-login");
+	private final String loginId = "passp-field-login";
+	private final String passwordId = "passp-field-passwd";
+	
+	private By passwordBy = By.id(passwordId);
+	private By loginBy = By.id(loginId);
 	private By submitBy = By.className("button2_type_submit");
+	private By inputBy = By.cssSelector("div.passp-form-field__input input");
 	
 	public SignInPage(WebDriver driver, WebDriverWait wait) {
 		super(driver, wait);
 	}
 	
-	public void signIn(String userLogin, String userPassword) {
-		this.enterText(loginBy, userLogin);
-		this.click(submitBy);
-		this.enterText(passwordBy, userPassword);
-		this.click(submitBy);
+	public void signIn(String userLogin, String userPassword) {	
+		String id = find(inputBy).getAttribute("id");
+		if (id.equals(loginId)) {
+			enterText(loginBy, userLogin);
+			click(submitBy);
+		}	
+		enterText(passwordBy, userPassword);
+		click(submitBy);
 	}
 }
